1216f0aafcd4844dde1cedb620bd182348551af8
hendricwidjaja <hcwidjaja@gmail.com>
2024-06-13 20:30:49 +1000

    Sets examples

--- sets.py
+++ sets.py
@@ -15,3 +15,21 @@ print(my_set)
 # Membership testing
 print(2 in my_set)
 print(3 in my_set)
+
+# Set operations
+set_a = {1, 2, 3}
+set_b = {3, 4, 5}
+
+# Union = set_a + set_b values. Therefore union of both sets would result with {1, 2, 3, 4, 5} NOTE: duplicate of 3 is removed as there can't be duplicate values in a set
+# Even though unordered, it will put numbers in ascending order
+# Union example
+union_value = set_b.union(set_a)
+print(union_value)
+
+# Intersection = trying to find an intersection of 2x sets, it will give you the common value/s in both sets. Above example would result in {3}. If there aren't any common values, it will result with an empty set.
+intersection_value = set_a.intersection(set_b)
+print(intersection_value)
+
+# Difference = set_a - set_b (it will remove any duplicate values. But will only provide values for first set). Above example would result in {1, 2}
+difference_value = set_a.difference(set_b)
+print(difference_value)
